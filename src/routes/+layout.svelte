bb60bbf6634fe2115731b4e6de1b9551bf601f1c
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-05-17 13:00:13 +0500

    script running

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
-	import '../app.css';
-	
-	let { children } = $props();
+  import "../app.css";
+
+  let { children } = $props();
 </script>
 
+<div class="fixed top-10 right-10 space-x-4">
+  <a href="/" class="link">Home</a>
+  <a href="/hello" class="link">Hello</a>
+</div>
 {@render children()}
